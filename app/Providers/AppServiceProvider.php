13deb105413e5714ed62f886c26473b14a089101
OA-SEONG-SIK\bbq50 <sosn3@naver.com>
2021-03-29 20:00:11 +0900

    헤더에 리스트 뿌리기 진행 중

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,10 +24,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $first_list = array(
+            "first"=>"첫번째 메뉴"
+            ,"seconde"=>"두번째 메뉴"
+            ,"third"=>"세번째 메뉴"
+        );
+
+        $seconde_list = array(
+            "third"=>array(
+                "one"=>"세번째에 첫번째"
+                ,"two"=>"세번째에 두번째"
+            )
+        );
+
         View::composer('common.header', function($view) {
             $view
                 ->with('title', 'Buchet Homepage')
-                ->with('description', '부쳇의 홈페이지이입니다.');
+                ->with('description', '부쳇의 홈페이지이입니다.')
+                // ->with('seconde_list',$seconde_list)
             ;
         });
     }
