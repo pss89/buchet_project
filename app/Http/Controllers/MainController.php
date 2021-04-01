5bf7aa8a586c5c9cfef1872a89efbc2a8dc88403
OA-SEONG-SIK\bbq50 <sosn3@naver.com>
2021-04-01 19:09:34 +0900

    수정

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,10 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\View\Composers\ProfileComposer;
 
+use App\Models\Common;
 class MainController extends Controller
 {
     public function mainMethod()
     {
+        
+
+        // $flights = Common::where('history_id', 1)
+        //        ->orderBy('user_id')
+        //     //    ->take(10)
+        //        ->get();
+
+        // echo "<pre>";
+        // print_r($flights);
+        // exit;
+
         return view('main/main',['datas'=>array('data','print')]);
     }
 }
---
+++ app/Models/Common.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Common extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tb_user';
+    // protected $connection = 'sqlite';
+}
